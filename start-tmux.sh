aeb194bc4c2b1397188ad2bf706e7f23a9802db1
Sreenath Madasu <sreenath.madasu@gmail.com>
2015-04-12 13:15:35 -0400

    autostart vim in tmux with parameter

--- start-tmux.sh
+++ start-tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export TERM=screen-256color-bce
-tmux new-session -d 'vim $1'
+tmux new-session -d
+tmux send-keys 'vim '$1 'Enter'
 tmux split-window -h
 tmux send-keys 'lein repl' 'Enter'
 tmux resize-pane -R 30
